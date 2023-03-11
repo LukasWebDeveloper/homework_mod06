c933effdc27942a8cca1fe07f20a92b38018c09a
Łukasz Duszak <lukas94.WebDeveloper@gmail.com>
2023-03-12 00:39:06 +0100

    index.html - all tags add

---
+++ js/script.js
@@ -0,0 +1,22 @@
+{
+    const listTasks = [
+        {
+            content : "Mod06",
+            done : false,
+        },
+        {
+            content : "leki",
+            done : true,
+        },
+    ];
+
+    const render = () => {
+        //teraz tu
+    }
+
+    init = () => {
+        render();
+    }
+
+    init();
+}
