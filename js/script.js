31f15a2f63ab0f39df462a578e159faa836a7b96
Łukasz Duszak <lukas94.WebDeveloper@gmail.com>
2023-03-15 00:33:50 +0100

    index.html - edit code

--- js/script.js
+++ js/script.js
@@ -1,22 +1,47 @@
+
 {
-    const listTasks = [
+    const tasks = [
         {
-            content : "Mod06",
-            done : false,
+            content: "Wystylować CSS listy zadań",
+            done: true,
         },
         {
-            content : "leki",
-            done : true,
+            content: "stworzyć button `toggle` zakończonego zadania",
+            done: true,
+        },
+        {
+            content: "stworzyć button `delete` zadania",
+            done: false,
         },
     ];
 
     const render = () => {
-        //teraz tu
-    }
+        let htmlString = "";
 
-    init = () => {
+        for(const task of tasks) {
+            htmlString += `
+                <li
+                    class="js-list__item ${task.done ? "js-list__item--done" : ""}"
+                >
+                    ${task.done 
+                        ? "<img class=js-greenToggle src=\"img/greenBird.png\">" 
+                        : "<img class=js-greenToggle src=\"img/greenFullColor.png\">"} 
+                
+                   ${task.content}
+                  
+                     <img class=js-redDelete src=img/redDelete.png>
+                   <hr>
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML= htmlString;
+
+    };
+
+    const init = () => {
         render();
-    }
-
+    };
+    
     init();
 }
